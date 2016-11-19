17d2c8de68efa09225e6b34fff006e8cfa5924f6
Khoi Le <khoileee89@gmail.com>
2016-11-19 16:38:45 -0500

    json data renders in view but unable to query correctly

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ angular
     Router
   ])
   .controller("HomeController", [
+    "HomeFactory",
     HomeControllerFunction
   ])
   .controller("SkinController", [
@@ -16,8 +17,23 @@ angular
   .controller("SpotlightController", [
     SpotlightControllerFunction
   ])
+  .factory("HomeFactory", [
+    "$resource",
+    HomeFactoryFunction
+  ])
+function Skin(){
+  this.skinss = ''
+}
+function HomeControllerFunction(HomeFactory) {
+  // this.colors = ["orange", "blue", "green"]
+  this.skins = HomeFactory.query((skins) => {
+    console.log(skins)
+  })
 
-function HomeControllerFunction() {
+
+    // skins.$save();
+// i need to be able access each object and  to print out only the names
+// the return data is an object with keys as "Jax" and value as objects. JSON file but values as objects
   console.log("Im using the Home Controller!")
 }
 function SkinControllerFunction(){
@@ -25,6 +41,28 @@ function SkinControllerFunction(){
 }
 function SpotlightControllerFunction(){
   console.log("I'm using the SPOTLIGHT controller")
+}
+function HomeFactoryFunction($resource) {
+// this is the champ by id, no images
+return $resource("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=RGAPI-fad0a8b6-b0e0-4393-b52f-136a9ee42cdc", {}, {
+  query: {
+    method: "GET",
+    params: {},
+    isArray: true,
+    transformResponse: function(data, header){
+      var jsonData = JSON.parse(data)
+      var skins = []
+      angular.forEach(jsonData, function(item) {
+        var skin = new Skin();
+        skin.skinss= item
+        skins.push(skin)
+      })
+    return skins;
+    }
+  }
+})
+  // return $resource("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/<CHAMPION-ID>?champData=image&api_key=<API-KEY>")
+  console.log("Home Factory being used")
 }
 function Router($stateProvider) {
   $stateProvider
