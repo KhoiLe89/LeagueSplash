2d5db04e6c6b88a490d1f77cd2f4eccaa136f39e
Khoi Le <khoileee89@gmail.com>
2016-11-20 15:26:28 -0500

    api data renders

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require("express")
 var app = express()
+var hbs = require("hbs")
+var request = require("request")
+
+app.set("view engine", "hbs")
 
 app.listen("3001", () => {
   console.log("express is working!")
 })
 
 app.get("/", (req, res) => {
-  res.send("LegendsSplash")
+  console.log("working")
+   var url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=RGAPI-fad0a8b6-b0e0-4393-b52f-136a9ee42cdc"
+   request(url, function(err, response, body) {
+     var championData = JSON.parse(body);
+     
+     res.render("home", {championData: championData})
+   })
 })
