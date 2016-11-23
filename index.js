4e52a184dbd49cd06b5a2c007235581633a94a72
Khoi Le <khoileee89@gmail.com>
2016-11-23 11:18:30 -0500

    linked a new array of champion names with each of their skins from skinData

--- index.js
+++ index.js
@@ -18,27 +18,63 @@ app.get("/", (req, res) => {
      var championData = JSON.parse(body);
      var championPic = "http://ddragon.leagueoflegends.com/cdn/6.22.1/img/champion/"
      var champions = championData.data
+     let championName = []
+     console.log(champions)
+     for (var i in champions){
+       var result = champions[i]
+       for (var prop in result){
+
+      if (prop === "name"){
+        championName.push(result[prop])
+        var unique = championName.filter(function(elem, index, self) {
+          return index == self.indexOf(elem);
+        })
+        // console.log(championName)
+        // console.log(unique)
+      }
+      else{
+        console.log("nope")
+      }
+       }
+     }
+
      Champion.find({}).then((skinData) => {
        let skinInfo = []
        skinData.forEach(function (result) {
-         console.log(result)
-         if (result.name == champions.name){
-           console.log("working")
-         }
-         else {
-           console.log(champions)
-         }
+        //  console.log(result)
+
          result.skins.forEach((skinArrayKeys) => {
 
-          //  console.log(skinArrayKeys.skinImg)
+           console.log(skinArrayKeys.skinImg)
            skinInfo.push(skinArrayKeys)
+
          })
-        //  console.log(skinImgs)
+         console.log(skinInfo)
+        for (var i in skinInfo){
+
+          for (var o in unique){
+            if (unique[o]== skinInfo[i].name){
+              console.log(unique[o] + " = " + skinInfo[i].skinImg) //unique[o] is each name in the array and skinInfo[i] is each object
+            }
+          }
+
+
+
+        }
+        // for (var i=0; i < unique.length; i++){
+        //
+        //   if(unique[i] == skinData.name ){
+        //     console.log(unique[i] + " = " + skinArrayKeys)
+        //   }
+        //   else{
+        //     console.log("didnt work")
+        //   }
+        // }
 
       })
 // i have the array full of the skinData now.
       // skinImgs.forEach()
-      res.render("home", {champions, championPic, skinData, skinInfo})
+      res.render("home", {champions, championPic, skinData, skinInfo, unique})
      })
    })
 })
--- notes.md
+++ notes.md
@@ -10,4 +10,9 @@ MongoDB
 for each element in the array, print out the key and value(Aatrox skin model from men-lab)
 
 Next Step:
-  I have access to the skins in express. Now I need to place a conditional if the Champion name = SkinObject.name, print out all skinImg
+  I have access to the skins in express. Now I need to place a conditional if the Champion name = SkinObject.name, print out all skinImg. What if i push only the names from the API into an array, then do a forEach on those names and compare those to the results.name? maybe i can set each element in the name array to its own name?
+
+
+I have each skin linked with each of the names in the name array.
+Now I have to print them out next to each name in the view.
+The problem now is that I can get the name of the name array to show, but the images from the API doesn't.
