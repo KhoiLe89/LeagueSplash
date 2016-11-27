9c198af87fb45056f1eff4d40c546392168e8929
Khoi Le <khoileee89@gmail.com>
2016-11-27 16:21:47 -0500

    layout created, some css changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-var hbs = require("hbs")
+var hbs = require("express-handlebars")
 var request = require("request")
 var Skin = require("./db/schema.js").Skin
 var Champion = require("./db/schema.js").Champion
@@ -8,21 +8,24 @@ var Champion = require("./db/schema.js").Champion
 
 app.set("view engine", "hbs")
 app.use("/assets", express.static("public"));
+app.engine('hbs', hbs({extname:'hbs', defaultLayout:'layout-main.hbs'}));
 
 app.listen("3001", () => {
   console.log("express is working!")
 })
 
 app.get("/champions", (req, res) => {
-  console.log("working")
    var url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=RGAPI-fad0a8b6-b0e0-4393-b52f-136a9ee42cdc"
    request(url, function(err, response, body) {
+     if(err) {
+       console.log("error", err)
+     }
      var championData = JSON.parse(body);
      var championPic = "http://ddragon.leagueoflegends.com/cdn/6.22.1/img/champion/"
      var champions = championData.data
      let championName = []
      let championPics = []
-     console.log(champions)
+
      //for champion names
      for (var i in champions){
        var result = champions[i]
@@ -47,7 +50,7 @@ app.get("/champions", (req, res) => {
             var uniquePics = championPics.filter(function(elem, index, self) {
               return index == self.indexOf(elem);
             })
-            console.log(uniquePics)
+
           }
           else{
             console.log("nope")
@@ -56,17 +59,13 @@ app.get("/champions", (req, res) => {
      }
 
      Champion.find({}).then((skinData) => {
+       console.log(champions)
        let skinInfo = []
        skinData.forEach(function (result) {
         //  console.log(result)
-
          result.skins.forEach((skinArrayKeys) => {
-
-           console.log(skinArrayKeys.skinImg)
            skinInfo.push(skinArrayKeys)
-
          })
-         console.log(skinInfo)
         for (var i in skinInfo){
 
           for (var o in unique){
@@ -78,8 +77,6 @@ app.get("/champions", (req, res) => {
           }
         }
       })
-
-
 
       res.render("home", {champions, championPic, skinData, skinInfo, unique, uniquePics})
      })
