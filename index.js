b0f45b51eefe039224bf74fa82e3cbac4c9f6ec6
Khoi Le <khoileee89@gmail.com>
2016-11-28 10:49:32 -0500

    testing bootstrap

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ app.engine('hbs', hbs({extname:'hbs', defaultLayout:'layout-main.hbs'}));
 app.listen("3001", () => {
   console.log("express is working!")
 })
-
+app.get("/test", (req, res) => {
+  res.render('test')
+})
 app.get("/champions", (req, res) => {
    var url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=RGAPI-fad0a8b6-b0e0-4393-b52f-136a9ee42cdc"
    request(url, function(err, response, body) {
-     if(err) {
-       console.log("error", err)
-     }
-     var championData = JSON.parse(body);
+     var championData = JSON.parse(body)
      var championPic = "http://ddragon.leagueoflegends.com/cdn/6.22.1/img/champion/"
      var champions = championData.data
      let championName = []
